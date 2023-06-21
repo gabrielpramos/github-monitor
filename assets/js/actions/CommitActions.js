1c5aca90a26064e107a28998c6e6339cba685924
Gabriel Pereira Ramos <gpr@CASCA-018423.cesar-ldap.intranet>
2023-06-21 15:57:35 -0300

    Create new reducer for repository list locally

--- assets/js/actions/CommitActions.js
+++ assets/js/actions/CommitActions.js
@@ -2,10 +2,15 @@ import * as types from './ActionTypes';
 
 export const createRepositorySuccess = (response, successMessage) => ({
   type: types.CREATE_REPOSITORY_SUCCESS,
-  payload: {response, successMessage},
+  payload: { response, successMessage },
 });
 
 export const getCommitsSuccess = commits => ({
   type: types.GET_COMMITS_SUCCESS,
   payload: commits,
 });
+
+export const createCommitsListSuccess = (response, succesMessage) => ({
+  type: types.CREATE_COMMITS_LIST,
+  payload: { response, succesMessage }
+})
